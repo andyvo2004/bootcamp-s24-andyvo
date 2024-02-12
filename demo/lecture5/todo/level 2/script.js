84e8a6badb07177f3580fb4678dfa5615257cd24
Andy Vo <andy.v.vo2004@gmail.com>
2024-02-11 20:46:29 -0500

    lecture 5 demo

--- demo/lecture5/todo/level 2/script.js
+++ demo/lecture5/todo/level 2/script.js
@@ -1,1 +1,24 @@
 //solution-
+
+document.getElementById('add-button').addEventListener("click", () => {
+    let todoList = document.getElementById("todo-list");
+    let todoInput = document.getElementById("todo-input");
+    let newTodo = todoInput.value;
+
+    if (newTodo.trim() !== "") {
+        let listItem = document.createElement("li");
+        let removeButton = document.createElement("buttom");
+        removeButton.className = "remove-button";
+        removeButton.textContent = "Remove";
+        removeButton.onclick = () => {
+            let todoList = document.getElementById("todo-list");
+            todoList.removeChild(listItem);
+        }
+        listItem.textContent = newTodo;
+        listItem.appendChild(removeButton);
+        todoList.appendChild(listItem);
+        todoInput.value = "";
+    } else {
+        alert("Please enter a task");
+    }
+})
